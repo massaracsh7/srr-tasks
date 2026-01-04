4647d39603b015210b63b4fe1ceb0d88862ac9d0
massaracsh7 <fatum7@gmail.com>
2026-01-04 17:03:29 +0300

    feat: add create course page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,22 @@ import { Login } from './pages/login/login';
 import { Profile } from './pages/profile/profile';
 import { CourseDetail } from './pages/course-detail/course-detail';
 import { Lesson } from './pages/lesson/lesson';
+import { authGuard } from './core/auth-guard';
+import { CreateCourse } from './pages/create-course/create-course';
 
-export const routes: Routes = [{ path: '', component: Home },
-{ path: 'login', component: Login },
-{ path: 'profile', component: Profile },
-{ path: 'course/:id', component: CourseDetail },
-  { path: 'course/:courseId/:lessonId', component: Lesson },
-{ path: '**', redirectTo: '' }
+
+export const routes: Routes = [
+  { path: '', component: Home },
+
+  { path: 'login', component: Login },
+
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
+
+  { path: 'course/:id', component: CourseDetail },
+
+  { path: 'course/:courseId/:lessonId', component: Lesson, canActivate: [authGuard] },
+
+  { path: 'create-course', component: CreateCourse, canActivate: [authGuard] },
+
+  { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/create-course/create-course.ts
@@ -0,0 +1,43 @@
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../core/user';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-course',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './create-course.html',
+  styleUrl: './create-course.scss',
+  standalone: true
+})
+export class CreateCourse {
+  userService = inject(UserService);
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    goals: [''],
+    lessons: ['']
+  });
+
+  saveCourse() {
+    const value = this.form.value;
+
+    const newCourse = {
+      title: value.title,
+      description: value.description,
+      goals: value.goals
+        ? value.goals.split(',').map((g: string) => g.trim()).filter(Boolean)
+        : [],
+      lessons: value.lessons
+        ? value.lessons.split(',').map((l: string) => l.trim()).filter(Boolean)
+        : []
+    };
+
+    console.log('Новый курс:', newCourse);
+    alert('Курс сохранён');
+
+    this.form.reset();
+  }
+}
