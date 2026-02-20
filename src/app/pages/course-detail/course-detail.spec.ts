67e2f256a922c2f55dc7b69a01de99843b1be76b
massaracsh7 <fatum7@gmail.com>
2026-02-20 16:11:23 +0300

    feat: add create-course page test

--- src/app/pages/course-detail/course-detail.spec.ts
+++ src/app/pages/course-detail/course-detail.spec.ts
@@ -1,23 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EnvironmentInjector, input, runInInjectionContext } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { Courses } from '../../core/courses';
+import { UserService } from '../../core/user';
 
 import { CourseDetail } from './course-detail';
 
 describe('CourseDetail', () => {
   let component: CourseDetail;
-  let fixture: ComponentFixture<CourseDetail>;
+
+  const coursesMock = {
+    getCourseById: vi.fn(),
+  };
+
+  const userServiceMock = {
+    currentUser: vi.fn(),
+  };
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CourseDetail]
-    })
-    .compileComponents();
+    vi.restoreAllMocks();
+    vi.spyOn(input, 'required').mockReturnValue((() => '1') as any);
 
-    fixture = TestBed.createComponent(CourseDetail);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    coursesMock.getCourseById.mockReturnValue({ id: 1, title: 'Angular 20 Basics' });
+
+    await TestBed.configureTestingModule({
+      providers: [
+        { provide: Courses, useValue: coursesMock },
+        { provide: UserService, useValue: userServiceMock },
+      ],
+    });
+
+    const injector = TestBed.inject(EnvironmentInjector);
+    component = runInInjectionContext(injector, () => new CourseDetail());
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize signals', () => {
+    expect(component.course()).toBeUndefined();
+    expect(component.progress()).toBeGreaterThanOrEqual(0);
+    expect(component.progress()).toBeLessThan(100);
   });
 });
